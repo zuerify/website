ccae4a8bee38324318df36f01673bfa26012381c
zuerify <dev@zuerify.com>
2022-12-04 14:49:45 +0100

    dev changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,6 @@
-<script lang="ts">
-	let dots = '.';
-
-	setInterval(() => {
-		if (dots.length == 3) {
-			return (dots = '.');
-		}
-
-		dots += '.';
-	}, 1000);
-</script>
-
 <div class="dark:bg-neutral-800 dark:text-stone-200 min-h-screen flex justify-center items-center">
 	<div>
 		<h3>Zuerify</h3>
-		<h1 class="transition-all">Coming Soon{dots}</h1>
+		<h1 class="transition-all">This is the development page</h1>
 	</div>
 </div>
