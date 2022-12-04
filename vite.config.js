9165e03c7ab7372dd24e3f3e1c708791df7b81e3
zuerify <dev@zuerify.com>
2022-12-04 14:57:34 +0100

    webfont

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import webfontDownload from 'vite-plugin-webfont-dl';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit(), SvelteKitPWA(), webfontDownload()],
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA(),
+		webfontDownload([
+			'https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
+		])
+	],
 
 	build: {
 		sourcemap: 'true'
