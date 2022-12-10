63f4a184aa75faef0c9d52f8be748a6efea1e44e
zuerify <dev@zuerify.com>
2022-12-10 18:20:51 +0100

    page content test

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -5,6 +5,6 @@
 <div class="flex flex-grow items-center justify-center ">
 	<div>
 		<h3>Zuerify</h3>
-		<h1>This is the development page</h1>
+		<h1>This is the development page 2</h1>
 	</div>
 </div>
