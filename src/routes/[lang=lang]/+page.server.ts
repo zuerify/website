61d07a06e1709d2e64597b5a6fcaf1f3d3e55348
zuerify <dev@zuerify.com>
2023-01-15 17:30:09 +0100

    removed prisma and prerender

--- src/routes/[lang=lang]/+page.server.ts
+++ src/routes/[lang=lang]/+page.server.ts
@@ -1,10 +1,7 @@
 import type { PageServerLoad } from './$types';
-import { db } from '$lib/server/prisma';
 
 export const load: PageServerLoad = async () => {
 	return {
 		//await db.user.findMany()
 	};
 };
-
-export const prerender = 'auto';
