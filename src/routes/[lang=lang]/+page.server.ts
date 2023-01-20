870519fc70c76f7af7d4d3aaabcb8a93eed9aa77
zuerify <dev@zuerify.com>
2023-01-20 21:37:33 +0100

    changed to await

--- src/routes/[lang=lang]/+page.server.ts
+++ src/routes/[lang=lang]/+page.server.ts
@@ -3,6 +3,6 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	return {
-		works: db.project.findMany()
+		works: await db.project.findMany()
 	};
 };
